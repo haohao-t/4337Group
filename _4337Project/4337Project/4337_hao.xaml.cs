650cb1b0b42cb54da06bd6b0ee4bf3bb7386088b
adel <adelina.fursova@mail.ru>
2025-02-18 23:39:35 +0300

    добавила события в свое окно

--- _4337Project/4337Project/4337_hao.xaml.cs
+++ _4337Project/4337Project/4337_hao.xaml.cs
@@ -1,16 +1,23 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using Microsoft.Win32; 
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace _4337Project
 {
@@ -22,6 +29,54 @@ namespace _4337Project
         public _4337_hao()
         {
             InitializeComponent();
+        }
+
+        private void Import_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                string connectionString = "data source=(localdb)\\MSSQLLocalDB;Initial Catalog=Status_hao;Integrated Security=True;";
+                string fileName = System.IO.Path.GetFileNameWithoutExtension(filePath);
+                string tableName = "Table_" + fileName;
+                try
+                {
+                    hao_Import_4var.ImportData(filePath, connectionString, tableName);
+                    MessageBox.Show("Данные успешно импортированы!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при импорте данных: {ex.Message}");
+                }
+            }
+        }
+
+        private void Export_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "data source=(localdb)\\MSSQLLocalDB;Initial Catalog=Status_hao;Integrated Security=True;";
+            string tableName = "Table_2";
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel Files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+            saveFileDialog.FileName = "ExportBD_hao4var.xlsx";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string outputFilePath = saveFileDialog.FileName;
+
+                try
+                {
+                    hao_Export_4var.ExportData(connectionString, tableName, outputFilePath);
+                    MessageBox.Show("Данные успешно экспортированы по статусу");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при экспорте данных: {ex.Message}");
+                }
+            }
         }
     }
 }
